6d4b1111468231b03d569a354171942ba214c61a
Joynul Abedin <joynulabedin510@gmail.com>
2025-04-24 00:27:18 +0600

    Changed code

---
+++ test.js
@@ -0,0 +1,14 @@
+
+// Object 
+
+    const Joy = {
+        name: "Joy",
+        age: 25,
+        greet: function() {
+        console.log("Hello, my name is ");
+            }   
+        }
+
+    Joy.greet(); 
+    console.log(Joy.name);
+    console.log(Joy["age"]);
